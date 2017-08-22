092634433659fdcfb7d41af2e9d102a4aaaf6a90
lecler_i <contact@thomas.sh>
2017-08-22 19:38:45 +0200

    Split raid and part into two files

--- src/ynh_part/part.py
+++ src/ynh_part/part.py
@@ -18,11 +18,6 @@
     along with this program; if not, see http://www.gnu.org/licenses
 
 """
-
-""" yunohost_part.py
-
-    IM DOING STUFFS
-"""
 import os
 import re
 import json
@@ -33,55 +28,12 @@ import requests
 import subprocess
 import parted
 import pyudev
-import pexpect
 import sys
 
 from moulinette.core import MoulinetteError
 from moulinette.utils.log import getActionLogger
 
 logger = getActionLogger('yunohost.part')
-
-class Mdadm(object):
-    def __init__(self):
-        pass
-
-    def create(self, device, level, devices_list, name = None, chunk = None, force = False):
-        errors = []
-        can_continue = False
-
-        args = ['--create', device]
-        if chunk:
-            args += ['-c', chunk]
-        if name:
-            args += ['-N', name]
-
-        args += ['-l', level, '-n', str(len(devices_list))]
-        args += devices_list
-        self.child = pexpect.spawn('mdadm', args)
-        #self.child.logfile = sys.stdout
-        try:
-            errors = self._parse_errors([])
-            if self.child.after.strip() == 'Continue creating array?':
-                can_continue = True
-                if force:
-                    self.child.sendline('Y')
-                    self.child.expect('Y\r\n')
-                    result = self._parse_errors([])
-                    ret = self.child.wait()
-                    return { 'warnings': errors, 'result': result, 'success': True if ret == 0 else False }
-        except(pexpect.EOF):
-            pass
-
-        return { 'errors': errors, 'can_continue': can_continue }
-
-    def _parse_errors(self, errors):
-        if self.child.expect(['^mdadm:[^\r]+\r\n', pexpect.EOF, 'Continue creating array\? ']) == 0:
-            error = self.child.after.strip()
-            if self.child.expect(['\s+[^\r]+\r\n', pexpect.EOF]) == 0:
-                error += ' - ' + self.child.after.strip()
-            errors.append(error)
-            return self._parse_errors(errors)
-        return errors
 
 class Fdisk(object):
     """Main program class"""
@@ -396,7 +348,7 @@ def humanize_sizeof(num, suffix='B'):
 ### Yunhost functions ###
 ###
 
-def ynh_part_list_all(auth = None):
+def part_list_all(auth = None):
     try:
         context = pyudev.Context()
         devices = [device.device_node for device in context.list_devices(DEVTYPE='disk') if device['MAJOR'] == '8' or device['MAJOR'] == '3']
@@ -466,10 +418,3 @@ def part_fresh_disk(devpath, auth = None):
         raise MoulinetteError(errno.EIO, e.message)
 
 
-def part_raid_create(device, level, devices, force=False, auth = None):
-    try:
-        mdadm = Mdadm()
-        ret = mdadm.create(device=device, level=level, devices_list=devices, force=force)
-        return ret
-    except Exception as e:
-        raise MoulinetteError(errno.EIO, e.message)
